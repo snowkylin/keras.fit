074192256091c089dc20543997e4c2c374e9d1c8
snowkylin <xihanli316@gmail.com>
2024-04-01 22:16:24 +0100

    add edit button

--- conf.py
+++ conf.py
@@ -47,3 +47,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = "furo"
 html_static_path = ['_static']
+
+html_theme_options = {
+    "source_repository": "https://github.com/snowkylin/keras.fit/",
+    "source_branch": "main",
+    "source_directory": "/",
+}
